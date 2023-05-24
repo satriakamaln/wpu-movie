c2979b5f983c41c23ff70b6c17c21da533388215
Satria Kamal Norsaid <satriakamaln@gmail.com>
2023-05-24 15:27:40 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function searchMovie() {
+    $('#movie-list').html('');
+
+    $.ajax({
+        url : 'http://www.omdbapi.com/',
+        type : 'get',
+        dataType : 'json',
+        data : {
+            'apikey' : 'c2595272',
+            's' : $('#search-input').val()
+        },
+        success : function (result) {
+            // console.log(result)
+            if (result.Response == "True") {
+
+                let movies = result.Search;
+                // console.log(movies)
+
+                $.each(movies, function (i, data){
+                    $('#movie-list').append(`
+                        <div class="col-md-4">
+                            <div class="card mb-3">
+                                <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                <h5 class="card-title">`+ data.Title +`</h5>
+                                <p class="card-text">`+ data.Year +`</p>
+                                <a href="#" class="card-link">See Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    `)
+                })
+
+            } else {
+                $('#movie-list').html(`
+                    <h1 class="col-md text-center">`+ result.Error +`</h1>
+                `)
+            }
+
+            $('#search-input').val('');
+        }
+    })
+}
+
+$('#search-button').on('click', function () {
+    searchMovie()
+})
